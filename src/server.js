6fb041adb9694972f457ed77faa574c2b75872bc
Thiago dos Santos <thidosantos@gmail.com>
2023-04-18 14:13:25 -0400

    add routes
    Criando file routes, para retira a route do arquivo principal do server.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
 import express from "express";
+import BaseRouter from "./routes/Base.js";
 
 const app = express();
 
 
-app.get('/', (req, res) => {
-    res.send('Oi tudo bem!');
-});
+app.use(BaseRouter);
 
 const port = 3001;
-
 app.listen(port, () => {
     console.log(`Server is running at ${port}`);
 });
